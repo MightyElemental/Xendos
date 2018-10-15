912253400fdd1cac577cb085f46ccfbad0ae7f01
James <James@James-PC.Home>
2018-10-15 18:52:47 +0100

    Added copyright notice to the start of each document

--- XendosXP/src/net/mightyelemental/winGame/OSSettings.java
+++ XendosXP/src/net/mightyelemental/winGame/OSSettings.java
@@ -2,6 +2,22 @@ package net.mightyelemental.winGame;
 
 import org.newdawn.slick.UnicodeFont;
 
+/**
+ * XendosXP - A custom operating system that runs in a window Copyright (C) 2018
+ * James Burnell
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 public class OSSettings {
 
 	private OSSettings() {
--- XendosXP/src/net/mightyelemental/winGame/programs/AppWebBrowser.java
+++ XendosXP/src/net/mightyelemental/winGame/programs/AppWebBrowser.java
@@ -9,6 +9,22 @@ import net.mightyelemental.winGame.guiComponents.GUITextBox;
 import net.mightyelemental.winGame.guiComponents.GUIhtmlViewer;
 import net.mightyelemental.winGame.guiComponents.dekstopObjects.AppWindow;
 
+/**
+ * XendosXP - A custom operating system that runs in a window Copyright (C) 2018
+ * James Burnell
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 public class AppWebBrowser extends AppWindow {
 
 	private static final long serialVersionUID = 7935648659277287522L;
