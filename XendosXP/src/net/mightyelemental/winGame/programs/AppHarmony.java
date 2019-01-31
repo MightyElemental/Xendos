ce62df9c8536a0415880b55edb8d4f91a4956479
James <James@James-PC.Home>
2019-01-31 17:59:32 +0000

    Removed new firebase api

--- XendosXP/src/net/mightyelemental/winGame/programs/AppHarmony.java
+++ XendosXP/src/net/mightyelemental/winGame/programs/AppHarmony.java
@@ -1,14 +1,6 @@
 package net.mightyelemental.winGame.programs;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
 import org.newdawn.slick.Graphics;
-
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.auth.FirebaseAuth;
 
 import net.mightyelemental.winGame.guiComponents.dekstopObjects.AppWindow;
 
@@ -32,24 +24,8 @@ public class AppHarmony extends AppWindow {
 
 	private static final long serialVersionUID = 1046548414901267080L;
 
-	private FirebaseAuth	fbAuth;
-	private FirebaseApp		fbApp;
-
 	public AppHarmony(float x, float y, float width, float height) {
 		super(x, y, width, height, "Harmony");
-
-		try {
-			//FileInputStream serviceAccount = new FileInputStream("path/to/serviceAccountKey.json");
-			FirebaseOptions options = new FirebaseOptions.Builder().setCredentials(GoogleCredentials.getApplicationDefault())
-					.setDatabaseUrl("https://xendos-chat.firebaseio.com/").build();
-
-			fbApp = FirebaseApp.initializeApp(options);
-			System.out.println(fbApp.getName());
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 
 	}
 
