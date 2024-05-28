a8f6adbd0c8c62d110db21aaca750c87f27db17a
MightyElemental <12786983+MightyElemental@users.noreply.github.com>
2024-05-28 16:19:00 +0100

    Removed phantomjs due to security flaws & upgraded to Selenium

--- XendosXP/src/net/mightyelemental/winGame/guiComponents/GUIhtmlViewer.java
+++ XendosXP/src/net/mightyelemental/winGame/guiComponents/GUIhtmlViewer.java
@@ -1,6 +1,9 @@
 package net.mightyelemental.winGame.guiComponents;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
@@ -9,31 +12,34 @@ import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.util.Log;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import com.google.common.io.Files;
 
 import net.mightyelemental.winGame.ResourceLoader;
 import net.mightyelemental.winGame.guiComponents.dekstopObjects.AppWindow;
 
 /**
- * XendosXP - A custom operating system that runs in a window Copyright (C) 2018
- * James Burnell
+ * XendosXP - A custom operating system that runs in a window Copyright (C) 2018 James Burnell
  * 
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, version 3 of the License.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, version 3 of the License.
  * 
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License along with
- * this program. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 public class GUIhtmlViewer extends GUIComponent {
 
 	private static final long serialVersionUID = 3497817514161328855L;
 
-	private Image	page;
+	private Image		page;
 	private boolean	loaded, loading;
 	private boolean	couldNotResolve;
 
@@ -42,17 +48,22 @@ public class GUIhtmlViewer extends GUIComponent {
 
 	private int tick;
 
-	public GUIhtmlViewer(float width, float height, String uid, AppWindow aw) {
+	/** The thread executor to parallelize the web rendering */
+	Executor executor;
+
+	public GUIhtmlViewer( float width, float height, String uid, AppWindow aw ) {
 		super(width, height, uid, aw);
 		nullImg = ResourceLoader.loadImage("webcache.null");
+		executor = Executors.newSingleThreadExecutor();
 	}
 
-	public void draw(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+	public void draw( GameContainer gc, StateBasedGame sbg, Graphics g ) throws SlickException {
 		super.draw(gc, sbg, g);
-		if ( page != null ) {
+		if (page != null) {
+			// TODO: Add scroll feature by rendering section of screenshot
 			g.drawImage(page.getScaledCopy((int) width, (int) height), x, y);
-		} else if ( loaded ) {
-			if ( ResourceLoader.imageExists("webcache/" + fileName) ) {
+		} else if (loaded) {
+			if (ResourceLoader.imageExists("webcache/" + fileName)) {
 				page = ResourceLoader.loadImage("webcache/" + fileName);
 				couldNotResolve = false;
 			} else {
@@ -64,13 +75,13 @@ public class GUIhtmlViewer extends GUIComponent {
 					new Color(0f, 0f, 0f, 0.5f));
 		}
 		g.setColor(Color.black);
-		if ( couldNotResolve ) {
+		if (couldNotResolve) {
 			g.drawString("Could not resolve webpage.\nEnsure you have typed the URL correctly.", 10, height - 15);
 		}
-		if ( !isLoaded() && !loading && !couldNotResolve ) {
+		if (!isLoaded() && !loading && !couldNotResolve) {
 			g.drawString("Provide a URL to get started", x + 5, y + 5);
 		}
-		if ( loading ) {
+		if (loading) {
 			animateLoading(g);
 			tick += 5;
 		} else {
@@ -78,7 +89,7 @@ public class GUIhtmlViewer extends GUIComponent {
 		}
 	}
 
-	private void animateLoading(Graphics g) {
+	private void animateLoading( Graphics g ) {
 		int oX = 20;
 		int oY = 20;
 		int scale = 7;
@@ -90,34 +101,73 @@ public class GUIhtmlViewer extends GUIComponent {
 				(float) (y + oY + scale * Math.cos(Math.toRadians(tick + 240))), 5, 5);
 	}
 
-	public void displayWebsite(String url) {
+	/**
+	 * Load a URL and render it to an image to be displayed.
+	 * 
+	 * @param url the desired website URL
+	 */
+	public void displayWebsite( String url ) {
 		fileName = url.replaceAll("[^A-Za-z0-9]", "");
 		loaded = false;
 		couldNotResolve = false;
 		page = null;
-		if ( ResourceLoader.imageExists("webcache/" + fileName) ) {
+		if (ResourceLoader.imageExists("webcache/" + fileName)) {
 			loaded = true;
 			page = ResourceLoader.loadImage("webcache/" + fileName);
 		} else {
-			new Thread() {
-				public void run() {
-					try {
-						loading = true;
-						Log.info("Requesting URL [" + url + "]");
-						Process process = new ProcessBuilder("./lib/3rdparty/phantomjs.exe", "/lib/3rdparty/rasterize.js", url,
-								"./assets/textures/webcache/" + fileName + ".png", "1280*720px").start();
-						process.waitFor();
-						Log.info("Loaded webpage [" + url + "]");
-						loaded = true;
-						if ( !ResourceLoader.imageExists("webcache/" + fileName) ) {
-							couldNotResolve = true;
-						}
-						loading = false;
-					} catch (IOException | InterruptedException e) {
-						e.printStackTrace();
-					}
+
+			executor.execute(() -> {
+
+				try {
+					WebDriver webDriver = getNewDriver();
+
+					loading = true;
+					Log.info("Requesting URL [" + url + "]");
+
+					screenshotWebsite(webDriver, url, fileName);
+					webDriver.quit();
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					loaded = true;
+					couldNotResolve = !ResourceLoader.imageExists("webcache/" + fileName);
+					loading = false;
 				}
-			}.start();
+
+			});
+
+		}
+	}
+
+	/**
+	 * Creates a new headless browser driver
+	 * 
+	 * @return A new headless driver
+	 */
+	public WebDriver getNewDriver() {
+		ChromeOptions co = new ChromeOptions();
+		co.addArguments("--headless");
+		co.addArguments(String.format("--window-size=%d,%d", (int) (this.width * 1.5f), (int) (this.height * 1.5f)));
+		WebDriver webDriver = new ChromeDriver(co);
+		return webDriver;
+	}
+
+	/**
+	 * Takes a screenshot of a website at a given URL using a given web driver.
+	 * 
+	 * @param webDriver the web driver used to take the screenshot
+	 * @param url the URL to screenshot
+	 * @param fileName the name of the image file to save as
+	 */
+	public void screenshotWebsite( WebDriver webDriver, String url, String fileName ) {
+		webDriver.get(url);
+		File screenshot = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+		webDriver.close();
+
+		try {
+			Files.move(screenshot, new File("./assets/textures/webcache/" + fileName + ".png"));
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
